5b3ee0ea5eeebbf3b4278ac04283e9de22c10a02
Bart Dopheide <dopheide@fmf.nl>
2025-10-31 22:59:14 +0100

    WIP

--- RoadFighterOverlay.tcl
+++ RoadFighterOverlay.tcl
@@ -4,7 +4,7 @@
 namespace eval road_fighter_overlay {
 
 variable road_fighter_overlay_active false
-variable text_height 8
+variable text_height 7
 
 variable game_states [list\
 	"Not playing"\
@@ -18,7 +18,7 @@ variable game_states [list\
 	"Reached checkpoint"
 ]
 variable game_substates [list\
-	[list "Game startup" "Scroll up Konami logo" "Show software" "Show/blink ROAD FIGHTER logo"]\
+	[list "Game startup" "Scroll up Konami logo" "Show software" "Show ROAD FIGHTER logo" "Blink ROAD FIGHTER logo"]\
 	[list "Waiting for input"]\
 	[list "Start demo?" "Playing demo"]\
 	[list\
@@ -45,47 +45,83 @@ variable game_substates [list\
 	]\
 	[list "Reached checkpoint" "Clear screen" "Show progress map" "Blink minicar" "Goto nextstage"]\
 ]
+variable car_states [list\
+	"All okay"\
+	"Crashed"\
+	"Out of fuel, with speed>0"\
+	"Reached checkpoint"\
+	"Skidding"\
+	"Bounce"\
+	"Respawn"\
+	"Spinning"\
+]
 proc init {} {
 	variable text_height
-	
+
 	## Create OSD master element. It autoscales the widgets.
 	osd_widgets::msx_init road_fighter
-	
+
 	osd create rectangle road_fighter.game_state\
-		-x 0x08 -y 0x00\
-		-w 0xC8 -h [expr {0x02 + $text_height}]\
+		-relx 0x08 -rely [expr {0 * (0x01 + $text_height)}]\
+		-relw 0xC8 -relh [expr {0x01 + $text_height}]\
 		-rgba 0x00000080
 	osd create text road_fighter.game_state.text -x 0 -y 0 -size $text_height -text ""
-	
+
 	osd create rectangle road_fighter.game_substate\
-		-relx 0x08 -rely [expr {0x00 + ($text_height} + 0x02)]\
-		-w 0xC8 -h [expr {0x02 + $text_height}]\
+		-relx 0x08 -rely [expr {1 * (0x01 + $text_height)}]\
+		-relw 0xC8 -relh [expr {0x01 + $text_height}]\
 		-rgba 0x00000080
 	osd create text road_fighter.game_substate.text -x 0 -y 0 -size $text_height -text ""
-	
-	osd create rectangle road_fighter.pos\
-		-relx 0x00 -w 0x10\
-		-rely 0x00 -h 0x10\
+
+	osd create rectangle road_fighter.car_state\
+		-relx 0x08 -rely [expr {2 * (0x01 + $text_height)}]\
+		-relw 0xC8 -relh [expr {0x01 + $text_height}]\
 		-rgba 0x00000080
-	
+	osd create text road_fighter.car_state.text -x 0 -y 0 -size $text_height -text ""
+
+	#osd create rectangle road_fighter.pos_box\
+	#	-relx 0x00 -relw 0x09\
+	#	-rely  666 -relh 0x10\
+	#	-rgba 0x00000060
+	osd_widgets::create_power_bar\
+		road_fighter.pos_box\
+		9 16\
+		0x00000000 0xff770020 0xff0000ff
+	osd create text road_fighter.pos_text\
+		-relx 666 -rely 666\
+		-size $text_height -text ""
+
 	osd create rectangle road_fighter.speed\
-		-x 0xE0 -y 0x80\
-		-w 0x13	-h $text_height\
+		-relx [expr { 0xC0 + 4 }] -rely 0x80\
+		-relw 0x13 -relh $text_height\
 		-rgba 0x00000080
-	osd create text road_fighter.speed.text -x 0 -y 0 -size $text_height -text ""
-	
+	osd create text road_fighter.speed.text\
+		-relx 0 -rely 0x20\
+		-size $text_height -text ""
+
 	osd create rectangle road_fighter.fuel\
-		-x 0xFC -y 0x80\
-		-w 0x13	-h $text_height\
+		-relx [expr { 0xD8 + 4 }] -rely 0x80\
+		-relw 0x13 -relh $text_height\
 		-rgba 0x00000080
-	osd create text road_fighter.fuel.text -x 0 -y 0 -size $text_height -text ""
-	
+	osd create text road_fighter.fuel.text\
+		-relx 0 -rely 0\
+		-size $text_height -text ""
+
+	osd create rectangle road_fighter.distance_travelled\
+		-relx [expr { 0xC8 + 2 }] -rely 0x70\
+		-relw 0x13 -relh $text_height\
+		-rgba 0x00000080
+	osd create text road_fighter.distance_travelled.text\
+		-relx 0 -rely 0\
+		-size $text_height -text ""
+
 	update_overlay
 }
 proc toggle_road_fighter_overlay {} {
 	variable road_fighter_overlay_active
 	set road_fighter_overlay_active [expr {!$road_fighter_overlay_active}]
 	if {$road_fighter_overlay_active} {
+		osd destroy road_fighter
 		init
 		set text "Road Fighter overlay activated!"
 	} else {
@@ -106,11 +142,11 @@ proc update_impl {} {
 	variable game_states
 	set text_color 0xffffff
 	set game_state [peek 0xE000]
-	set text [format "   State 0x%02X: %s" $game_state\
+	set text [format "Game state 0x%02X: %s" $game_state\
 		[lindex $game_states $game_state]]
 	osd configure road_fighter.game_state.text\
 		-text $text -rgb $text_color
-	
+
 	## Game substate
 	variable game_substates
 	set text_color 0xffffff
@@ -118,37 +154,63 @@ proc update_impl {} {
 	set game_substate [peek 0xE001]
 	set game_substate_text\
 		[lindex [lindex $game_substates $game_state] $game_substate]
-	set text [format "Substate 0x%02X: %s" $game_substate $game_substate_text]
+	set text [format "Game substate 0x%02X: %s" $game_substate $game_substate_text]
 	osd configure road_fighter.game_substate.text\
 		-text $text -rgb $text_color
 	#if {$game_substate_text == ""} { debug break }
-	
+
+	## Car state.
+	variable car_states
+	set text_color 0xffffff
+	set car_state [peek 0xE049]
+	set car_state_text\
+		[lindex $car_states $car_state]
+	set text [format "Car state %d: %s" $car_state $car_state_text]
+	osd configure road_fighter.car_state.text\
+		-text $text -rgb $text_color
+
+
 	## Car position
-	#set hpos [peek 0xE04E]
-	#set vpos [peek 0xE04C]
-	#set hpos [peek 0xE113]
-	#set vpos [peek 0xE112]
-	set hpos [peek 0xE11B]
-	set vpos [peek 0xE11A]
-	osd configure road_fighter.pos -relx $hpos -rely $vpos
+	set vpos [peek 0xE112]
+	set hpos [peek 0xE113]
+	osd configure road_fighter.pos_box\
+		-relx [expr {$hpos+4}]\
+		-rely [expr {$vpos+1}]
+	set pos [format "(%d,%d)" $hpos $vpos]
+	set text_color 0xff0000
+	#osd configure road_fighter.pos_text\
+	#	-relx [expr {$hpos+4}]\
+	#	-rely [expr {$vpos+1+16}]\
+	#	-text $pos -rgb $text_color
+	osd configure road_fighter.pos_box.text\
+		-text $pos
 
 	## Speed
-	set speed [format "0x%02X" [peek 0xE04F]]
+	set speed [format "#%02X" [peek 0xE04F]]
 	set text_color 0xffffff
 	if {$speed == 0xD7} {set text_color 0xff0000}
 	osd configure road_fighter.speed.text\
 		-text $speed -rgb $text_color
-	
+
 	## Fuel
-	set fuel [format "0x%02X" [peek 0xE083]]
+	set fuel [format "#%02X" [peek 0xE083]]
 	set text_color 0xffffff
 	if {$fuel <= 0x09} {set text_color 0xffa500}
 	if {$fuel <= 0x00} {set text_color 0xff0000}
 	osd configure road_fighter.fuel.text\
 		-text $fuel -rgb $text_color
+
+	## Distance travelled.
+	set distance_travelled [format "#%06X" [expr {[peek_u16 0xE077] | [peek_u8 0xE079] << 16}]]
+	set text_color 0xffffff
+	osd configure road_fighter.distance_travelled.text\
+		-text $distance_travelled -rgb $text_color
 }
 
 namespace export toggle_road_fighter_overlay
 };## namespace road_fighter_overlay
 
 namespace import road_fighter_overlay::*
+
+## Set to on.
+toggle_road_fighter_overlay
