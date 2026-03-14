3265c238c5ac26c01dc20eb2589ff495521a905b
Bart Dopheide <3164970+dopheideb@users.noreply.github.com>
2026-03-14 22:34:29 +0100

    Create dump for in the disassembly.

--- remake/track.py
+++ remake/track.py
@@ -95,26 +95,49 @@ if __name__ == '__main__':
 				band=band
 			)
 
-	## Track patterns 0xD5 upto and including 0xD8. Mirror source: patterns 0xD1 upto and including 0xD4.
+	## Track patterns 0xD5 upto and including 0xD8. Mirror source: 
+	## patterns 0xD1 upto and including 0xD4.
 	konami.mirror_VRAM_patterns_in_vertical_axis(
 		source=mem.get_word(0x68CE + 1),
 		destination=mem.get_word(0x68D1 + 1),
 		num=mem.get_byte(0x68D4 + 1)
 	)
 
-	## Track patterns 0xE1 upto and including 0xE8. Mirror source: patterns 0xD9 upto and including 0xE0.
+	## Track patterns 0xE1 upto and including 0xE8. Mirror source: 
+	## patterns 0xD9 upto and including 0xE0.
 	konami.mirror_VRAM_patterns_in_vertical_axis(
 		source=mem.get_word(0x68F4 + 1),
 		destination=mem.get_word(0x68F7 + 1),
 		num=mem.get_byte(0x68FA + 1)
 	)
 
-	## Track patterns 0xF3 upto and including 0xF8. Mirror source: patterns 0xEC upto and including 0xF1.
+	## Track patterns 0xF3 upto and including 0xF8. Mirror source: 
+	## patterns 0xEC upto and including 0xF1.
 	konami.mirror_VRAM_patterns_in_vertical_axis(
 		source=mem.get_word(0x691A + 1),
 		destination=mem.get_word(0x691D + 1),
 		num=mem.get_byte(0x6920 + 1)
 	)
+
+	colored_characters = vdp.get_colored_characters_3D()
+	color2char = np.array([\
+			'=',		## 0. Transparent
+			'?',		## 1. Black
+			' ',		## 2. Medium green
+			'?',		## 3. Light green
+			'?',		## 4. Dark blue
+			'?',		## 5. Light blue
+			'?',		## 6. Dark red
+			'?',		## 7. Cyan
+			'?',		## 8. Medium red
+			'?',		## 9. Light red
+			'?',		## 10. Dark yellow
+			'?',		## 11. Light yellow
+			'?',		## 12. Dark green
+			'?',		## 13. Magenta
+			'.',		## 14. Grey
+			'x',		## 15. White
+	])
 
 	elements = \
 	[
@@ -132,29 +155,64 @@ if __name__ == '__main__':
 		},
 	]
 	last_name = 0x3800
+	handled = {}
 	for element in elements:
 		start = mem.get_word(0x593A + 2 * element['index'])
-		last = start
+		current = start
 
 		num_row_of_tracks = 8
 		num_rows_seen = 0
 		while num_rows_seen < num_row_of_tracks:
+			addr = current
 			track_names = mem[
-				last
+				addr
 				:
-				last + element['num_columns']
+				addr + element['num_columns']
 			]
 			if track_names[0] == 0xFF:
-				last = start
+				current = start
 				continue
 			num_rows_seen += 1
-			last += element['num_columns']
+			current += element['num_columns']
 
 			num_indent = 11 - element['num_columns']
 			for n, track_name in enumerate(track_names):
 				vdp.WRTVRM(last_name + num_indent + n, track_name)
-			## Advance to next VPP line
+
+			## Advance to next VDP line.
 			last_name += 0x20
+
+			## Dump patterns as text.
+			if addr in handled:
+				continue
+			handled[addr] = True
+			dump =\
+			{
+				'address': 'None',
+				'header': "\t;",
+				'pattern': [
+					f"\t;",
+					f"\t;",
+					f"\t;",
+					f"\t;",
+					f"\t;",
+					f"\t;",
+					f"\t;",
+					f"\t;",
+				],
+				'footer': f"\tdefb",
+			}
+			for n, track_name in enumerate(track_names):
+				dump['header'] += f"  [{n:X}] 0x{addr + n:04X}: 0x{track_name:02X}"
+				for m, chars in enumerate(color2char[colored_characters[track_name]]):
+					dump['pattern'][m] += ' | ' + ' '.join(chars)
+				dump['footer'] += f"{' ' if n == 0 else ','}0x{track_name:02X}"
+
+			print(dump['header'])
+			for pat in dump['pattern']:
+				print(pat + ' |')
+			print(dump['footer'] + f"\t;{addr:04x}")
+			print()
 
 	all_names = np.tile(np.arange(256), 3)	## [0, ..., 255, 0, ..., 255, 0, ..., 255]
 	vdp.color_register = mem.get_byte(0x46B0)
